70c195f870b17e7871d7fc2e20c9bc7735811009
Andrew Caya <andrewscaya@yahoo.ca>
2021-07-29 20:06:56 -0400

    2021-07-29 AC: Updates the project's package creation script.

--- data/filebrowser_package_create.bash
+++ data/filebrowser_package_create.bash
@@ -13,7 +13,10 @@ tar -C /srv --exclude='filebrowser/.github' \
 --exclude='filebrowser/.php_cs' \
 --exclude='filebrowser/.phpunit.result.cache' \
 --exclude='filebrowser/babel.config.js' \
+--exclude='filebrowser/CHANGELOG.md' \
+--exclude='filebrowser/CODE_OF_CONDUCT.md' \
 --exclude='filebrowser/configuration.php' \
+--exclude='filebrowser/CONTRIBUTING.md' \
 --exclude='filebrowser/couscous.yml' \
 --exclude='filebrowser/cypress.json' \
 --exclude='filebrowser/data' \
@@ -26,3 +29,15 @@ tar -C /srv --exclude='filebrowser/.github' \
 --exclude='filebrowser/repository/.gitignore' \
 -czvf /srv/filebrowser-$FILEBROWSER_PKG_VERSION.tar.gz \
 filebrowser/
+mkdir -p /srv/zip
+cd /srv/zip || exit 1
+tar -xvf /srv/filebrowser-$FILEBROWSER_PKG_VERSION.tar.gz
+zip -r filebrowser-$FILEBROWSER_PKG_VERSION.zip filebrowser/
+mv filebrowser-$FILEBROWSER_PKG_VERSION.zip ..
+cd /srv || exit 1
+rm -rf zip/
+mv filebrowser-$FILEBROWSER_PKG_VERSION.tar.gz /srv/filebrowser/data
+mv filebrowser-$FILEBROWSER_PKG_VERSION.zip /srv/filebrowser/data
+chown 1000:1000 /srv/filebrowser/data/filebrowser-$FILEBROWSER_PKG_VERSION.tar.gz
+chown 1000:1000 /srv/filebrowser/data/filebrowser-$FILEBROWSER_PKG_VERSION.zip
+cd
