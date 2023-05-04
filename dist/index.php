62c115e6fef33e041ac1be1dfb6e2a5b3e4fbab2
Andrew Caya <andrewscaya@yahoo.ca>
2023-05-04 16:39:28 -0400

    2023-05-04 AC: Updates the main copyright notice.

--- dist/index.php
+++ dist/index.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the FileBrowser package.
  *
- * Copyright 2021, Foreach Code Factory <services@linuxforphp.com>
+ * Copyright 2021, LfPHP Services <services@linuxforphp.com>
  * Copyright 2018-2021, Milos Stojanovic <alcalbg@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
